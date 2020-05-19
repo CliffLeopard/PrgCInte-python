cb6b1fc9e4de517a158d951009b063200b554b5d
CliffLeopard <cliffleopard@yahoo.com>
2020-05-20 01:50:57 +0800

    添加使用物品协作型过滤推荐电影

--- src/chapter2/recommendations.py
+++ src/chapter2/recommendations.py
@@ -9,6 +9,7 @@ def top_matches(data, person="Tony", n=5, similarity=sim_pearson):
     return scores[0:n]
 
 
+# 根据用户的协作过滤
 # 根据评论者相似度作为权重，推荐电影
 def get_recommendations(data, person, similarity=sim_pearson):
     totals = {}
@@ -47,3 +48,35 @@ def transform_profs(data):
     return result
 
 
+# 基于物品的协作过滤
+# 构造物品比较数据集
+def calculate_similar_items(data, n=10):
+    result = {}
+    items = transform_profs(data)
+    for item in items:
+        scores = top_matches(items, item, n=n, similarity=sim_pearson)
+        result[item] = scores
+    return result
+
+
+# 基于物品的协作型过滤获得推荐电影
+def get_recommended_items(data, similar_items, user):
+    user_ratings = data[user]
+    scores = {}
+    total_sim = {}
+    for (item, rating) in user_ratings.items:
+        for (similarity, item2) in similar_items[item]:
+            # 如果该用户已经对当前电影进行过评价，则跳过
+            if item2 in user_ratings:
+                continue
+
+            # 评价值与相似度的加权和
+            scores.setdefault(item2, 0)
+            scores[item2] += similarity * rating
+
+            # 全部相似度之和
+            total_sim.setdefault(item2, 0)
+            total_sim[item2] += similarity
+    rankings = [(score / total_sim[item], item) for item, score in scores.items()]
+    rankings.reverse()
+    return rankings
